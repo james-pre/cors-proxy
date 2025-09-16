3c4f8c08eb429ac7adb51229613d27fa58cac867
James Prevett <jp@jamespre.dev>
2025-09-16 13:22:06 -0500

    Rewrite to ESM, remove broken deps

--- bin.js
+++ bin.js
@@ -1,57 +1,91 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-const {spawn} = require('child_process')
-const kill = require('tree-kill')
-const minimisted = require('minimisted')
 
-async function main({_: [cmd], p, d}) {
-  switch (cmd) {
-    case 'start': {
-      if (d) require('daemonize-process')()
-      const cmd = require.resolve('micro/bin/micro.js')
-      const args = [
-        cmd,
-        `--listen=tcp://0.0.0.0:${p || 9999}`
-      ]
-      let server = spawn(
-        'node', args,
-        {
-          stdio: 'inherit',
-          windowsHide: true,
-          cwd: __dirname
-        }
-      )
-      fs.writeFileSync(
-        path.join(process.cwd(), 'cors-proxy.pid'),
-        String(process.pid),
-        'utf8'
-      )
-      process.on('exit', server.kill)
-      return
-    }
-    case 'stop': {
-      let pid
-      try {
-        pid = fs.readFileSync(
-          path.join(process.cwd(), 'cors-proxy.pid'),
-          'utf8'
-        );
-      } catch (err) {
-        console.log('No cors-proxy.pid file')
-        return
-      }
-      pid = parseInt(pid)
-      console.log('killing', pid)
-      kill(pid, (err) => {
-        if (err) {
-          console.log(err)
-        } else {
-          fs.unlinkSync(path.join(process.cwd(), 'cors-proxy.pid'))
-        }
-      })
-    }
-  }
+import { spawn } from 'node:child_process';
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'node:fs';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
+import { parseArgs } from 'node:util';
+import handleRequest from './index.js';
+
+const temp = tmpdir();
+
+const {
+	positionals: [cmd],
+	values: { port, help, pid: pidFile },
+} = parseArgs({
+	options: {
+		help: { type: 'boolean', short: 'h', default: false },
+		port: { type: 'string', short: 'p', default: '9999' },
+		pid: { type: 'string', default: join(temp, 'cors-proxy.pid') },
+	},
+	allowPositionals: true,
+});
+
+if (cmd == 'help' || help || !cmd) {
+	console.log(`Usage: ${process.argv0} [...options] <command>
+
+Commands:
+    run                 Run the CORS proxy server in the foreground
+    start               Start the CORS proxy server daemon
+    stop                Stop the CORS proxy server daemon
+
+Options:
+    -h, --help          Show this help message
+    -p, --port <port>   Port to listen on (default: 9999)
+    --pid <path>        Path to PID file (default: ${join(temp, 'cors-proxy.pid')}
+`);
 }
 
-minimisted(main)
+switch (cmd) {
+	case 'run': {
+		const server = createServer(handleRequest);
+		server.listen(port, () => console.log('Listening on port', port));
+		process.on('exit', () => {
+			console.log('Shutting down server');
+			server.close();
+		});
+		break;
+	}
+
+	case 'start': {
+		if (existsSync(pidFile)) {
+			let pid;
+			try {
+				pid = parseInt(readFileSync(pidFile, 'utf8'));
+			} catch (e) {
+				console.error('Found existing PID file but could not read it');
+				process.exit(13);
+			}
+
+			try {
+				process.kill(pid, 0);
+			} catch {
+				console.error(`Server is already running (pid is ${pid})`);
+				process.exit(16);
+			}
+
+			console.error('Removing stale PID file');
+			unlinkSync(pidFile);
+		}
+
+		const daemon = spawn(
+			process.execPath,
+			['run', port && `--port=${port}`].filter((a) => a),
+			{
+				stdio: 'ignore',
+				detached: true,
+			},
+		);
+		daemon.unref();
+		console.log('Started CORS proxy server with PID', daemon.pid);
+		writeFileSync(pidFile, daemon.pid.toString());
+		process.exit(0);
+	}
+	case 'stop':
+		try {
+			const pid = parseInt(readFileSync(pidFile, 'utf8'));
+			process.kill(pid);
+		} catch (e) {
+			console.error('Invalid or missing PID file');
+		}
+}
